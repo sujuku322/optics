940f210b56d4f926acb7ebf27a480c62d1fadb58
sujuku322 <sujuku322@gmail.com>
2025-04-16 18:33:57 +0800

    確認方程是否成立，繪製函數圖(更新

--- Optics Fifth Edition (Eugene Hecht)/CH2-Example/main.py
+++ Optics Fifth Edition (Eugene Hecht)/CH2-Example/main.py
@@ -4,17 +4,18 @@ from main_verify import verify_wave_equation
 from main_fig import plot_wave_function
 
 x, t, v, a, b = sp.symbols('x t v a b', real=True, positive=True)
-psi = sp.exp(-4*a*x**2 - b*t**2 + 4*sp.sqrt(a*b)*x*t)
-#psi3 = sp.sin(x - v * t )
+#psi = sp.exp(-4*a*x**2 - b*t**2 + 4*sp.sqrt(a*b)*x*t)
+psi = sp.sin(x - v * t )
 
+#verify_wave_equation(psi, x, t, v, a, b)
 verify_wave_equation(psi, x, t, v, a, b)
-#verify_wave_equation(psi3, x, t, v, a, b)
 
-def psi(x, t, a=0.05, b=1):
-    return np.exp(-4*a*x**2 - b*t**2 + 4*np.sqrt(a*b)*x*t)  # 高斯脈衝波
-
+#def psi(x, t, a=0.05, b=1):
+#    return np.exp(-4*a*x**2 - b*t**2 + 4*np.sqrt(a*b)*x*t)  # 高斯脈衝波
+def psi(x, t, v=3):
+    return np.sin( x - v * t)
 plot_wave_function(psi, x)
-#plot_wave_function(psi3)
+
 
 '''
 def psi3(x, t, v=1):
--- Optics Fifth Edition (Eugene Hecht)/CH2-Example/main_fig.py
+++ Optics Fifth Edition (Eugene Hecht)/CH2-Example/main_fig.py
@@ -7,9 +7,9 @@ def plot_wave_function(psi, x, times=[0, 1, 2, 3]):
     繪製波形函數 psi(x, t)
 
     參數：
-    - psi_func: 你要繪圖的函數 (例如 psi(x, t))
+    - psi: 你要繪圖的函數 (例如 psi(x, t))
     - v: 波速
-    - x_range: 要畫圖的 x 範圍
+    - x: 要畫圖的 x 範圍
     - times: 要畫的時間點列表
     """
     x = np.linspace(-10, 10, 400)
